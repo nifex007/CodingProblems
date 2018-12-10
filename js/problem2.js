328efaf2b91e36bfbc9194fdeb083183ef6436da
Nifemi <nsolaojo@guineainsurance.com>
2018-12-10 18:03:50 +0100

    renamed function in problem2 to computeArrayProduct

--- js/problem2.js
+++ js/problem2.js
@@ -4,7 +4,7 @@ Given an array of integers, return a new array such that each element at index i
 For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
 **/
 
-function compute(arr){
+function computeArrayProduct(arr){
 	var returned_arr = [];
 	for(var i=0; i<=arr.length - 1; i++){
 		var product = 1;					
@@ -17,9 +17,9 @@ function compute(arr){
 			returned_arr.push(product);
 			 // console.log(returned_arr);
 		}
-		
+
 		return returned_arr;
 }
 
 
-console.log(compute([3, 2, 1]));
+console.log(computeArrayProduct([3, 2, 1]));
